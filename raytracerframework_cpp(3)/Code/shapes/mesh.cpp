98b3ca7f956c06d90558964fedb037d6e1487ec8
Mario Holubar <m.holubar@student.rug.nl>
2018-02-25 14:53:34 +0100

    Some comments

--- raytracerframework_cpp(3)/Code/shapes/mesh.cpp
+++ raytracerframework_cpp(3)/Code/shapes/mesh.cpp
@@ -5,9 +5,10 @@
 
 Hit Mesh::intersect(Ray const &ray)
 {
-	// iterate through triangles to find closest intersection
 	double t = std::numeric_limits<double>::infinity();
 	Vector N;
+	
+	// iterate through all triangles in the model to find the closest intersection
 	for (Triangle &face : faces) {
 		Hit hit = face.intersect(ray);
 		if (hit.t < t) {
@@ -15,14 +16,19 @@ Hit Mesh::intersect(Ray const &ray)
 			N = hit.N;
 		}
 	}
+	
+	// if t is unchanged, the ray does not hit any triangles
 	if (t == std::numeric_limits<double>::infinity()) return Hit::NO_HIT();
+	
     return Hit(t, N);
 }
 
 Mesh::Mesh(std::string const &model, Point pos, double scale) {
-	// convert mesh to triangles and apply translation and scaling
+	// load .obj file into a vector of vertices
 	OBJLoader loader(model);
 	std::vector<Vertex> verts = loader.vertex_data();
+	
+	// convert groups of three vertices to triangles while applying translation and scaling
 	for (std::vector<Vertex> ::iterator it = verts.begin(); it < verts.end(); ++it) {
 		Vector p0(it->x * scale + pos.x, it->y * scale + pos.y, it->z * scale + pos.z);
 		it++;
--- raytracerframework_cpp(3)/Code/shapes/plane.cpp
+++ raytracerframework_cpp(3)/Code/shapes/plane.cpp
@@ -2,14 +2,17 @@
 
 Hit Plane::intersect(Ray const &ray)
 {
+	// check if plane and ray are parallel to avoid dividing by 0
     if((ray.D.normalized()).dot(N) == 0)
         return Hit::NO_HIT();
 
+	// if they're not parallel, there is always a point t at which the ray meets the plane
     double t = (-O - (ray.O).dot(N))/((ray.D).dot(N));
 
+	// check if intersection point is behind the observer
     if (t<0.0)
         return Hit::NO_HIT();
-
+	
     return Hit(t, N);
 }
 
