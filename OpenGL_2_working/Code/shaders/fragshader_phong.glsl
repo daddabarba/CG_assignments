9c39e92e60e433d2b65e4796227593454229b79e
D. Barbieri <s3090078@fwn-bborg-edu-74-14.fmnsedu.rug.nl>
2018-03-02 19:27:36 +0100

    phong shading corrected (with fragment location)

--- OpenGL_2_working/Code/shaders/fragshader_phong.glsl
+++ OpenGL_2_working/Code/shaders/fragshader_phong.glsl
@@ -10,11 +10,27 @@ in vec3 vertNorm;
 // Specify the Uniforms of the fragment shaders
 // uniform vec3 lightPosition; // for example
 
+uniform vec3 lightPos;
+uniform vec3 lightCol;
+uniform vec3 objCol;
+uniform vec3 material;
+
 // Specify the output of the fragment shader
 // Usually a vec4 describing a color (Red, Green, Blue, Alpha/Transparency)
 out vec4 fNorm;
 
 void main()
 {
-    fNorm = vec4(normalize(vertNorm), 1.0)/2.0;
+    vec3 P = vec3(gl_FragCoord);
+
+    vec3 IA = objCol*material.x;
+
+    vec3 L = normalize(lightPos-P);
+    vec3 ID = objCol*lightCol*(material.y)*max(0.0,dot(L,vertNorm));
+
+    vec3 R = 2*dot(vertNorm,L)*vertNorm - L;
+    vec3 V = normalize(vec3(0.0,0.0,0.0) - P);
+    vec3 IS = lightCol*material.z*max(0.0,dot(R,V));
+
+    fNorm = vec4(IA+ID+IS, 1.0);
 }
